d7160f3ab4b2aed787a34e36f667c43ab6a2fb31
KelvinNatal <kelvinnatal@hotmail.com>
2023-10-05 00:45:58 -0300

    feat: add validation error

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import './shared/services/TranslationsYup';
+
 import { router } from './routes';
 
 const server = express();
 
+server.use(express.json());
+
 server.use(router);
 
-server.use(express.json());
 
 export { server };
