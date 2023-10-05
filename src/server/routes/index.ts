ccd22ee265bcb473b600b2a034f83d05f169c0f0
KelvinNatal <kelvinnatal@hotmail.com>
2023-10-04 23:57:16 -0300

    feat: added cidade controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import { CidadesController } from './../controllers';
+
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
     return res.status(StatusCodes.OK).send('Olá DEV');
 });
+
+router.post('/cidades', CidadesController.create);
 
 export { router };
