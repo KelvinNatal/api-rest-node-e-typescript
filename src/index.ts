aad2b56ee4b609a9cd02b7c918c6907d64f3b636
KelvinNatal <kelvinnatal@hotmail.com>
2023-10-08 12:34:09 -0300

    fix: production problem resolved

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/server';
 import 'dotenv/config';
 
-server.listen(process.env.PORT || 3500, () => console.log('App rodando'));
+const startServer = () => {
+    server.listen(process.env.PORT || 3500, () => console.log('App rodando'));
+};
+
+if(process.env.IS_LOCALHOST !== 'true'){
+    Knex.migrate.latest()
+        .then(() => {
+            startServer();
+        })
+        .catch(console.log);
+}else{
+    startServer();
+}
